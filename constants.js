9bca6f8b9ff958acacf74d5479e0c423eca132cd
Abhishek <76660005+abhishek213-alb@users.noreply.github.com>
2021-11-07 19:25:28 +0530

    Hospichat

---
+++ constants.js
@@ -0,0 +1,55 @@
+const prompts = [
+  [" Dr. Patel, can you tell me which pill to take"],
+  ["Thank you so much I really appriecate you helping us You are a hero, thanks for saving my life  "],
+  ["ballu"],
+  ["Good Morning Dr Patel", "good", "fun", "wonderful", "fantastic", "cool"],
+    ["help", "I Need help"],
+    ["headache","I","a","medicine","need"],
+    ["Ok, but I will need a prescription", "prescription", "okay", "nice"],
+    ["Thats great doctor, what else does this app do", "app"],
+    ["he"],
+    ["Wow","that’s great thank you doctor."],
+    ["no","not sure","maybe","no thanks"],
+    [""],
+    ["","ha","no","he","funny","joke"]
+
+  ]
+
+  
+  
+
+
+
+const replies = [
+  ["Hello Doctor!", "Hi!", "Hey!", "Hi there!","Howdy"],
+  [
+    "Fine... how are you?",
+    "Pretty well, how are you?",
+    "Fantastic, how are you?"
+  ],
+  [
+    "Nothing much",
+    "About to go to sleep",
+    "Can you guess?",
+    "I don't know actually"
+  ],
+  ["Hey shyam How are you"],
+  ["Hello, This is Dr. Patel and you can reach out to me here  "],
+  ["I would suggest you to take rest and sleep but if the problem persists for long then take tylenol "],
+  ["Don’t worry Shayam, I am a verified doctor on the healthchat app so just showing our chat is at the pharmacy, you will get the required medicines."],
+  ["This app can help you talk with me without coming to my cabin and there is 24*7 support so if there is any emergency then we can guide you to the right steps. This is a developing application and has many features upcoming."],
+  ["I would suggest you to take rest and sleep but if the problem persists for long then take tylenol "]
+]
+
+
+const alternative = [
+  
+  //"Don’t worry Shayam, I am a verified doctor on the healthchat app so just showing our chat is at the pharmacy, you will get the required medicines ",
+ //"I would suggest you to take rest and sleep but if the problem persists for long then take tylenol",//
+  "This app can help you talk with me without coming to my cabin and there is 24*7 support so if there is any emergency then we can guide you to the right steps. This is a developing application and has many features upcoming. "
+
+
+]//
+
+
+const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
---
+++ doc.js
@@ -0,0 +1,62 @@
+// Options the user could type in
+const prompts = [
+    ["Hello Dr. Patel, can you tell me which pill to take"],
+
+    ["i love you"]["i love you"],
+    ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+    ["bad", "bored", "tired"],
+    ["help me", "tell me story", "tell me joke"],
+    ["ah", "yes", "ok", "okay", "nice"],
+    ["bye", "good bye", "goodbye", "see you later"],
+    ["what should i eat today"],
+    ["bro"],
+    ["what", "why", "how", "where", "when"],
+    ["no","not sure","maybe","no thanks"],
+    [""],
+    ["haha","ha","lol","hehe","funny","joke"]
+  ]
+  
+  // Possible responses, in corresponding order
+  
+  const replies = [
+    ["Hello Doctor!", "Hi!", "Hey!", "Hi there!","Howdy"],
+    [
+      "Fine... how are you?",
+      "Pretty well, how are you?",
+      "Fantastic, how are you?"
+    ],
+    [
+      "Nothing much",
+      "About to go to sleep",
+      "Can you guess?",
+      "I don't know actually"
+    ],
+    ["I am infinite"],
+    ["I am just a bot", "I am a bot. What are you?"],
+    ["The one true God, JavaScript"],
+    ["I am nameless", "I don't have a name"],
+    ["I love you too", "Me too"],
+    ["Have you ever felt bad?", "Glad to hear it"],
+    ["Why?", "Why? You shouldn't!", "Try watching TV"],
+    ["What about?", "Once upon a time..."],
+    ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+    ["Bye", "Goodbye", "See you later"],
+    ["Sushi", "Pizza"],
+    ["Bro!"],
+    ["Great question"],
+    ["That's ok","I understand","What do you want to talk about?"],
+    ["Please say something :("],
+    ["Haha!","Good one!"]
+  ]
+  
+  // Random for any other user input
+  
+  const alternative = [
+    "Same",
+    "I Need some medicine..."
+  
+  ]
+  
+  // Whatever else you want :)
+  
+  const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
